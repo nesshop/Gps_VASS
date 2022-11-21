d7180e480e08c720ec1489902cd2e035cdd0acd0
VASS\ernesto.muros <ernesto.muros@vass.es>
2022-11-21 09:56:50 +0100

    Añado un togglebutton, con un boton para para la marcha, para borrar la ruta y para sacar una foto.

--- app/src/main/java/com/practicas/vass/gps_vass/MainActivity.kt
+++ app/src/main/java/com/practicas/vass/gps_vass/MainActivity.kt
@@ -1,48 +1,39 @@
 package com.practicas.vass.gps_vass
 
 import android.Manifest
-import android.content.Context
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
-import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
-import android.provider.Settings
-import android.text.method.TextKeyListener.clear
-import android.widget.TextView
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.location.LocationManagerCompat.getCurrentLocation
-import com.google.android.gms.common.util.MapUtils
 import com.google.android.gms.location.*
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
 import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.Polyline
-import com.google.android.gms.maps.model.PolylineOptions
-import com.practicas.vass.gps_vass.MapUtils.addPolyline
 import com.practicas.vass.gps_vass.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButtonClickListener,
     OnMyLocationClickListener {
 
-
-
-    private lateinit var tvLatitude: TextView
-    private lateinit var tvLongitude: TextView
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+
     private var locations = mutableListOf<LatLng>()
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
     private var locationCallback = object :LocationCallback(){
         override fun onLocationResult(result: LocationResult) {
@@ -50,153 +41,67 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButton
             result.locations.run {
                 this.forEach{locations.add(LatLng(it.latitude, it.longitude))}
 
-               com.practicas.vass.gps_vass.MapUtils.addPolyline(map,locations)
+               MapUtils.addPolyline(map,locations)
             }
         }
+
     }
-
-    private val coordinates = LatLng(37.0251, -3.6230)
-
-    companion object {
-        const val REQUEST_CODE_LOCATION = 0
-    }
-
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        createFragment()
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        tvLatitude = binding.tvLatitude
-        tvLongitude = binding.tvLongitude
-        binding.startRoute.setOnClickListener {
+        binding.btnStart.setOnClickListener {
 
             if (::map.isInitialized) {
-                clear()
-                getCurrentLocation()
+
                 fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
                 val locationRequest = LocationRequest.create()
                     .setInterval(5_000)
                     .setFastestInterval(2_000)
                     .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
-                fusedLocationProviderClient.requestLocationUpdates( locationRequest, locationCallback, Looper.getMainLooper())
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
             }
+            map.clear()
+            locations.clear()
+        }
+        binding.btnErase.setOnClickListener {
+            map.clear()
+            locations.clear()
+        }
+
+        binding.btnStop.setOnClickListener {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
 
         }
 
-        createFragment()
-    }
+        binding.btnPhoto.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
 
-
-    private fun getCurrentLocation() {
-
-        if (isLocationPermissionGranted()) {
-            if (isLocationEnabled()){
-
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task ->
-                    val location:Location? = task.result
-                    if (location == null){
-                        Toast.makeText(this, "Recibido un nulo", Toast.LENGTH_SHORT).show()
-                    }else{
-                        Toast.makeText(this, "Exitoso", Toast.LENGTH_SHORT).show()
-                        tvLatitude.text = "La longitud es ${location.latitude}"
-                        tvLongitude.text = "La latitud es ${location.longitude}"
-
-                    }
-
-                }
-
-            }else{
-                Toast.makeText(this,"Activa la localización", Toast.LENGTH_SHORT).show()
-                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-                startActivity(intent)
-            }
-
-        }else{
-            requestLocationPermission()
         }
-
-
     }
-
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager =
-            getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
 
     private fun createFragment() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-        com.practicas.vass.gps_vass.MapUtils.getDestination()
-        com.practicas.vass.gps_vass.MapUtils.getOrigin()
-        //createMarker()
-        //createPolylines()
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        map.isMyLocationEnabled = true
+        enableLocation()
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMyLocationClickListener(this)
-        enableLocation()
 
-    }
-
-    private fun createMarker() {
-        val coordinates = LatLng(40.416640, -3.703902)
-        val marker = MarkerOptions().position(coordinates).title("Mi humilde morada")
-        map.addMarker(marker)
-        map.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(coordinates, 20f),
-            4000,
-            null
-        )
-    }
-
-    private fun createPolylines() {
-        val polylineOptions = PolylineOptions()
-            .add(LatLng(37.0243684776579, -3.6232383048608767))
-            .add(LatLng(37.0241457695855, -3.62379620433569))
-            .add(LatLng(37.02384596922572, -3.6235708987784108))
-            .add(LatLng(37.024085809608195, -3.6230344569755175))
-            .width(15f)
-            .color(ContextCompat.getColor(this, R.color.purple_200))
-
-        val polyline = map.addPolyline(polylineOptions)
-
-        polyline.isClickable = true
-        map.setOnPolylineClickListener { polyline -> changeColorLine(polyline) }
-    }
-
-    fun changeColorLine(polyline: Polyline) {
-        val color = (0..3).random()
-        when (color) {
-            0 -> polyline.color = ContextCompat.getColor(this, R.color.teal_200)
-            1 -> polyline.color = ContextCompat.getColor(this, R.color.purple_700)
-            2 -> polyline.color = ContextCompat.getColor(this, R.color.teal_700)
-            3 -> polyline.color = ContextCompat.getColor(this, R.color.purple_500)
-        }
     }
 
     private fun isLocationPermissionGranted() = ContextCompat.checkSelfPermission(
@@ -204,6 +109,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButton
         Manifest.permission.ACCESS_FINE_LOCATION
     ) == PackageManager.PERMISSION_GRANTED
 
+    
+    @SuppressLint("MissingPermission")
     private fun enableLocation() {
         if (!::map.isInitialized) return
         if (isLocationPermissionGranted()) {
@@ -216,20 +123,16 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButton
     private fun requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(
                 this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        ) {
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
             Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
         } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_CODE_LOCATION
-            )
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_CODE_LOCATION)
         }
 
     }
 
+    @SuppressLint("MissingSuperCall", "MissingPermission")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -249,30 +152,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMyLocationButton
         }
     }
 
-    override fun onResumeFragments() {
-        super.onResumeFragments()
-        if (!::map.isInitialized) return
-        if (!isLocationPermissionGranted()) {
-            map.isMyLocationEnabled = false
-            Toast.makeText(
-                this,
-                "Para activar la localización ve a ajustes y acepta los permisos",
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-    }
-
     override fun onMyLocationButtonClick(): Boolean {
         Toast.makeText(this, "Botón pulsado", Toast.LENGTH_SHORT).show()
-        map.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(coordinates, 20f),
-            4000,
-            null
-        )
         return false
     }
 
-    override fun onMyLocationClick(p0: Location) {
-        Toast.makeText(this, "Estás en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
+    override fun onMyLocationClick(location: Location) {
+        Toast.makeText(this, "Estás en ${location.latitude}, ${location.longitude}", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/practicas/vass/gps_vass/MapUtils.kt
+++ app/src/main/java/com/practicas/vass/gps_vass/MapUtils.kt
@@ -2,18 +2,14 @@ package com.practicas.vass.gps_vass
 
 import android.content.Context
 import android.graphics.Color
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.JointType
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PolylineOptions
-import com.google.android.gms.maps.model.RoundCap
+import com.google.android.gms.maps.model.*
 
 object MapUtils {
 
-    fun getDestination(): LatLng = LatLng(37.0251, -3.6230)
-
-    fun getOrigin(): LatLng = LatLng(37.0238, -3.62336)
+    private fun getDestination(): LatLng = LatLng(37.0251, -3.6230)
 
     fun setupMap (context: Context, map:GoogleMap){
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(getDestination(),15f))
@@ -36,6 +32,42 @@ object MapUtils {
             .addAll(locations)
         )
 
+    }
+
+    fun createPolylines(map: GoogleMap) {
+        val polylineOptions = PolylineOptions()
+            .add(LatLng(37.0243684776579, -3.6232383048608767))
+            .add(LatLng(37.0241457695855, -3.62379620433569))
+            .add(LatLng(37.02384596922572, -3.6235708987784108))
+            .add(LatLng(37.024085809608195, -3.6230344569755175))
+            .width(15f)
+            .color(Color.RED)
+
+        val polyline = map.addPolyline(polylineOptions)
+
+        polyline.isClickable = true
+        map.setOnPolylineClickListener { polyline -> changeColorLine(polyline) }
+    }
+
+   private fun changeColorLine(polyline: Polyline) {
+        val color = (0..3).random()
+        when (color) {
+            0 -> polyline.color = Color.BLUE
+            1 -> polyline.color = Color.GREEN
+            2 -> polyline.color = Color.CYAN
+            3 -> polyline.color = Color.MAGENTA
+        }
+    }
+
+    private fun createMarker(map: GoogleMap) {
+        val coordinates = LatLng(40.416640, -3.703902)
+        val marker = MarkerOptions().position(coordinates).title("Mi humilde morada")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates, 20f),
+            4000,
+            null
+        )
     }
 
 
